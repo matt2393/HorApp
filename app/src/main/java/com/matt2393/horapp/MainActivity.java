679b12f61d182e3cb9b1dffcbd86586bfcb72eaa
matt2393 <mattct2393@gmail.com>
2018-10-02 18:17:12 -0400

    Initial commit

---
+++ app/src/main/java/com/matt2393/horapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.matt2393.horapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
