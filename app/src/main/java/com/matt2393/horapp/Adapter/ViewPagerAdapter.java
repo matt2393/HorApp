e1b8c5a1ed1d614fae99c0f8c89eda4dbd69a787
matt2393 <mattct2393@gmail.com>
2018-10-23 18:29:57 -0400

    Adicion de toolbar, ViewPager y TabLayout

---
+++ app/src/main/java/com/matt2393/horapp/Adapter/ViewPagerAdapter.java
@@ -0,0 +1,22 @@
+package com.matt2393.horapp.Adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
