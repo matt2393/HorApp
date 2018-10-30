6d19b11fdf86859c0d14a6b248e91a26d07d8fd8
matt2393 <mattct2393@gmail.com>
2018-10-30 18:02:31 -0400

    Funciona viewPager con fragmetDia

--- app/src/main/java/com/matt2393/horapp/Adapter/ViewPagerAdapter.java
+++ app/src/main/java/com/matt2393/horapp/Adapter/ViewPagerAdapter.java
@@ -1,22 +1,38 @@
 package com.matt2393.horapp.Adapter;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.matt2393.horapp.Fragments.FragmentDia;
+
+import java.util.ArrayList;
+
 public class ViewPagerAdapter extends FragmentPagerAdapter {
 
-    public ViewPagerAdapter(FragmentManager fm) {
+    private ArrayList<FragmentDia> dias;
+    private ArrayList<String> titulos;
+
+    public ViewPagerAdapter(FragmentManager fm, ArrayList<FragmentDia> dias, ArrayList<String> tits) {
         super(fm);
+        this.dias=dias;
+        this.titulos=tits;
     }
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+        return dias.get(position);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titulos.get(position);
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return dias.size();
     }
 }
--- app/src/main/java/com/matt2393/horapp/MainActivity.java
+++ app/src/main/java/com/matt2393/horapp/MainActivity.java
@@ -1,19 +1,47 @@
 package com.matt2393.horapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.matt2393.horapp.Adapter.ViewPagerAdapter;
+import com.matt2393.horapp.Fragments.FragmentDia;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar=findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        viewPager = findViewById(R.id.viewpager);
+        tabLayout = findViewById(R.id.tablayout);
 
+        ArrayList<FragmentDia> dias = new ArrayList<>();
+        dias.add(new FragmentDia());
+        dias.add(new FragmentDia());
+        dias.add(new FragmentDia());
+        dias.add(new FragmentDia());
 
+        ArrayList<String> tits = new ArrayList<>();
+        tits.add("Lunes");
+        tits.add("Martes");
+        tits.add("Miercoles");
+        tits.add("Jueves");
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(
+                getSupportFragmentManager(), dias, tits);
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
